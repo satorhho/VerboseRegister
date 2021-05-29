ef25709606729e6e288132be2f893b0cb4f54250
giaaanmari <gmramos@gbox.adnu.edu.ph>
2021-05-29 23:42:22 +0800

    stock data not showing

--- lib/Confirmation.dart
+++ lib/Confirmation.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'PendingOrders.dart';
 
 class confirmation extends StatelessWidget {
+  final databaseReference = FirebaseDatabase.instance.reference();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +20,36 @@ class confirmation extends StatelessWidget {
               ),
             ),
           ),
-          OutlineButton(
-            child: Text("View Orders"),
-            highlightColor: Colors.white38,
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => PendingOrders()),
-              );
-            },
+          Container(
+            child: OutlineButton(
+              child: Text("View Orders"),
+              highlightColor: Colors.white38,
+              onPressed: () {
+                List<String> templist = [];
+                List<dynamic> templistval = [];
+                List<String> status_list = [];
+                List<String> oid_list = [];
+
+                var future1 = this.databaseReference.child("Order").once().then((DataSnapshot snapshot){
+                  Map<dynamic, dynamic> value = snapshot.value;
+                  value.forEach((key, values) {
+                    templist.add(key);
+                    templistval.add(values);
+                  });
+                });
+
+                future1.then((value) {
+                  templistval.forEach((element) {
+                    oid_list.add(element["order_id"]);
+                    status_list.add(element["status"]);
+                  });
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PendingOrders(oid_list, status_list)),
+                  );
+                });
+              },
+            ),
           ),
         ]));
   }
--- lib/PendingOrders.dart
+++ lib/PendingOrders.dart
@@ -1,10 +1,23 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:verbose_register/OrderListContent.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 
 class PendingOrders extends StatelessWidget {
+  List<dynamic> order_ids;
+  List<dynamic> statuses;
+
+
+  PendingOrders(List<String> orderIds, List<String> statusList){
+    this.order_ids = orderIds;
+    this.statuses = statusList;
+
+    print(order_ids);
+    print(statuses);
+  }
+
   get fontSize => null;
   final databaseReference = FirebaseDatabase.instance.reference();
   @override
@@ -13,7 +26,6 @@ class PendingOrders extends StatelessWidget {
         body: Column(
           children: [
             Container(
-
               margin: const EdgeInsets.only(top: 50, bottom: 50),
               alignment: Alignment.center,
               child: Text(
@@ -36,39 +48,88 @@ class PendingOrders extends StatelessWidget {
                 ),
               ],
             ),
-            Row(
-              children: [
-                Container(
-                  width: 100,
-                  height: 50,
-                  margin: const EdgeInsets.only(left: 60, top: 20),
-                  child: OutlineButton(
-                    child: Text("OID1"),
-                    highlightColor: Colors.white38,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => OrderListContent()),
-                      );
-                      createRecord();
-                    },
-                  ),
-                ),
-              ],
+            Expanded(
+                child: Row(
+                  children: [
+                    // FOR ORDER ID
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: this.order_ids.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            child: OutlineButton(
+                              child: Text('${this.order_ids[index]}'),
+                              highlightColor: Colors.white38,
+                              onPressed: () {
+                                List<String> templist1 = [];
+                                List<dynamic> templistval1 = [];
+                                List<String> oid_list = [];
+                                List<String> sid_list = [];
+                                List<String> s_name = [];
+                                List<String> s_desc = [];
+
+                                var future1 = this.databaseReference.child("Stock")
+                                    .once()
+                                    .then((DataSnapshot snapshot) {
+                                  Map<dynamic, dynamic> value = snapshot.value;
+                                  value.forEach((key, values) {
+                                    templist1.add(key);
+                                    templistval1.add(values);
+
+                                  });
+
+                                });
+
+                                future1.then((value) {
+                                  templistval1.forEach((element) {
+                                    oid_list.add(element["order_id"]);
+                                    sid_list.add(element["stock_id"]);
+                                    s_name.add(element["name"]);
+                                    s_desc.add(element["description"]);
+                                  });
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) =>
+                                        OrderListContent(oid_list, sid_list,s_name,s_desc)),
+                                  );
+                                });
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+
+                    //FOR STATUS
+
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: this.statuses.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            height: 50,
+                            child: Center(child: Text('${this.statuses[index]}')),
+                          );
+                        },
+                      ),
+                    ),
+
+                  ],
+                )
             ),
           ],
         )
     );
   }
-    void createRecord() {
-      databaseReference.child("Order").set({
-        'order_id': 'OID1',
-        'status': 'Pending'
-      });
-    }
     void getData(){
       databaseReference.once().then((DataSnapshot snapshot) {
         print('${snapshot.value}');
       });
     }
+
+  void checkConfirmed(int index) {
+    if (this.statuses[index] == "Confirmed") {
+      return null;
+    }
+  }
 }
